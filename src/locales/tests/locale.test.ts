fd43a5c9aa6bda30e8d2df9053a0bda4e9347cf9
hannah <hannah.leong@digital.cabinet-office.gov.uk>
2023-10-18 14:15:45 +0100

    ATO-79: creating translations for the error page
     - welsh 404 errors
     - english 404 errors
     - create test to compare translation
    
    Co-authored-by: Isaac Au isaac.au@digital.cabinet-office.gov.uk

---
+++ src/locales/tests/locale.test.ts
@@ -0,0 +1,62 @@
+import { expect } from "chai";
+import { describe } from "mocha";
+import englishTranslations from "../en/translation.json";
+import welshTranslations from "../cy/translation.json";
+
+export function traverseObjectProperties(
+  obj: any,
+  func: (key: string, value?: string) => void
+): any {
+  const properties = Object.keys(obj);
+  properties.forEach((i) => {
+    if (obj[i] === Object(obj[i])) {
+      return traverseObjectProperties(obj[i], func);
+    }
+    func(i, obj[i]);
+  });
+}
+
+export function testExpectation(key: string, value: string): void {
+  expect(value).to.not.contain(
+    "'",
+    "translated values should not contain typographically incorrect quotes (aka 'straight quotes')"
+  );
+}
+
+describe("locale files", () => {
+  it("should not contain any typographically incorrect quotes (aka 'straight quotes') in Welsh translations", () => {
+    traverseObjectProperties(welshTranslations, testExpectation);
+  });
+  it("should not contain any typographically incorrect quotes (aka 'straight quotes') in English translations", () => {
+    traverseObjectProperties(englishTranslations, testExpectation);
+  });
+});
+
+describe("locale file structure", () => {
+  const welshTranslationFileKeys: string[] = [];
+  const englishTranslationFileKeys: string[] = [];
+
+  traverseObjectProperties(welshTranslations, (key) => {
+    welshTranslationFileKeys.push(key);
+  });
+
+  traverseObjectProperties(englishTranslations, (key) => {
+    englishTranslationFileKeys.push(key);
+  });
+
+  it("should be that locale files have the same number of keys", () => {
+    expect(englishTranslationFileKeys.length).to.equal(
+      welshTranslationFileKeys.length
+    );
+  });
+
+  it("should be that locale files have the same keys when sorted", () => {
+    expect([...englishTranslationFileKeys].sort()).to.eql(
+      [...welshTranslationFileKeys].sort()
+    );
+  });
+
+  it("should be that locale files have the same keys in the same position", () => {
+    expect(englishTranslationFileKeys).to.eql(welshTranslationFileKeys);
+  });
+});
