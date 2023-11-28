706622a0b51efa92da3293bef89802fd8ab0edc3
cargil <carly.gilson@digital.cabinet-office.gov.uk>
2023-11-28 17:26:40 +0000

    ATO-165: Add requests config

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,14 @@ export function supportLanguageCY(): boolean {
   return process.env.SUPPORT_LANGUAGE_CY === "1";
 }
 
+export function getApiKey(): string {
+  return process.env.API_KEY;
+}
+
+export function getApiBaseUrl(): string {
+  return process.env.API_BASE_URL;
+}
+
 export function getLogLevel(): string {
   return process.env.LOGS_LEVEL || "debug";
 }
--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,3 +1,13 @@
+export class ApiError extends Error {
+  private status?: number;
+  private data?: string;
+  constructor(message: string, status?: number, data?: string) {
+    super(message);
+    this.data = data;
+    this.status = status;
+  }
+}
+
 export function createServiceRedirectErrorUrl(
   redirectUri: string,
   error: string,
