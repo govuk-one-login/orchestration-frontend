04869a084957f22a6bf0025a2c01f9a5abe50373
Bill Dunn <bill.dunn@digital.cabinet-office.gov.uk>
2023-12-04 09:59:50 +0000

    ATO-214: Use CONTAINER-IMAGE-PLACEHOLDER as ECR image reference
    - When deploying through the pipeline, CONTAINER-IMAGE-PLACEHOLDER is replaced with the appropriate ECR image ref.
    - Update deploy-sandpit.sh to replace CONTAINER-IMAGE-PLACEHOLDER with sandpit ECR image ref.

--- deploy-sandpit.sh
+++ deploy-sandpit.sh
@@ -16,10 +16,19 @@ echo "Pushing image..."
 docker push "${REPO_URL}:${IMAGE_TAG}"
 
 pushd "./infrastructure/sandpit/ecs"
+if grep -q "CONTAINER-IMAGE-PLACEHOLDER" template.yaml; then
+    echo "Replacing \"CONTAINER-IMAGE-PLACEHOLDER\" with ECR image ref"
+    cp template.yaml template_tmp.yaml
+    sed -i '' "s|CONTAINER-IMAGE-PLACEHOLDER|$REPO_URL:$IMAGE_TAG|" template_tmp.yaml
+else
+    echo "ERROR: Image placeholder text \"CONTAINER-IMAGE-PLACEHOLDER\" not found"
+    exit 1
+fi
 echo ${parameters}
 echo "Deploying ECS"
 sam build
-sam deploy --stack-name sandpit-orch-frontend --template-file template.yaml --parameter-overrides $parameters --no-fail-on-empty-changeset --capabilities CAPABILITY_NAMED_IAM
+sam deploy --stack-name sandpit-orch-frontend --template-file template_tmp.yaml --parameter-overrides $parameters --no-fail-on-empty-changeset --capabilities CAPABILITY_NAMED_IAM
 echo "Updating service"
 aws ecs update-service --cluster sandpit-orch-app-cluster --service sandpit-orch-frontend-ecs-service --force-new-deployment --no-cli-pager
+rm template_tmp.yaml
 popd
