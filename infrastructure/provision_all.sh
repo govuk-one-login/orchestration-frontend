a38f8352ffe458588da97f8340eda9807a5dbd4f
Isaac Au <isaac.au@digital.cabinet-office.gov.uk>
2024-05-10 07:40:16 +0100

    ATO-469: Add deployment of dev ECR
     - added new provision_dev script
     - excluded dev specific ECR stack in provision_all script as it will cause error from naming
     - provision_dev script should be a temporary script until we move dev into our own account.

--- infrastructure/provision_all.sh
+++ infrastructure/provision_all.sh
@@ -9,7 +9,7 @@ export AWS_PAGER=""
 ## Provision dependencies
 for dir in configuration/"$AWS_ACCOUNT"/*/; do
   STACK=$(basename "$dir")
-  if [[ $STACK != "$AWS_ACCOUNT-orch-fe-pipeline" && -f configuration/$AWS_ACCOUNT/$STACK/parameters.json ]]; then
+  if [[ $STACK != "$AWS_ACCOUNT-orch-fe-pipeline" && $STACK != "orch-container-image-repository" &&  -f configuration/$AWS_ACCOUNT/$STACK/parameters.json ]]; then
     $PROVISION_COMMAND "$AWS_ACCOUNT" "$STACK" "$STACK" LATEST &
   fi
 done
